94f42f69ac13f2c57e4402fd9a9c833cd022cb1b
Zoey <53253298+luvHaze@users.noreply.github.com>
2020-09-21 23:41:23 +0900

    LinearLayout, GridLayout, Constraint Layout, RelativeLayout Frame Layout완료
    * Table Layout 하나 남음
    * Toolbar에서 Title과 Logo 전환 방법 찾아야 함

--- app/src/main/java/luv/zoey/layoutsample/MainActivity.kt
+++ app/src/main/java/luv/zoey/layoutsample/MainActivity.kt
@@ -2,15 +2,58 @@ package luv.zoey.layoutsample
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.MenuItem
+import android.view.View
+import androidx.navigation.NavController
+import androidx.navigation.NavHostController
+import androidx.navigation.findNavController
 import androidx.navigation.fragment.findNavController
 import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.google.android.material.navigation.NavigationView
 import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), BottomNavigationView.OnNavigationItemSelectedListener {
+
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        bottom_navigation_view.setupWithNavController(nav_host_fragment_container.findNavController())
+        bottom_navigation_view.setupWithNavController(findNavController(R.id.nav_host_fragment_container))
+        bottom_navigation_view.setOnNavigationItemSelectedListener(this)
     }
+
+    override fun onBackPressed() {
+        //super.onBackPressed()
+
+    }
+
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        Log.d("[LOG]", "$item.title")
+
+        when(item.title) {
+            "홈" -> {
+                findNavController(R.id.nav_host_fragment_container).navigate(R.id.homeFragment2)
+                return true
+            }
+            "카테고리" -> {
+                findNavController(R.id.nav_host_fragment_container).navigate(R.id.categoryFragment)
+                return true
+            }
+            "마이컬리" -> {
+                findNavController(R.id.nav_host_fragment_container).navigate(R.id.myKurlyFragment2)
+                return true
+            }
+            "검색" -> {
+                findNavController(R.id.nav_host_fragment_container).navigate(R.id.searchFragment)
+                return true
+            }
+        }
+        return false
+    }
+
+
 }
--- app/src/main/java/luv/zoey/layoutsample/fragment/MyKurlyFragment.kt
+++ app/src/main/java/luv/zoey/layoutsample/fragment/MyKurlyFragment.kt
@@ -1,9 +1,18 @@
 package luv.zoey.layoutsample.fragment
 
+import android.os.Bundle
+import android.view.View
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import kotlinx.android.synthetic.main.activity_main.*
 import luv.zoey.layoutsample.R
 
 
 class MyKurlyFragment : Fragment(R.layout.fragment_mykurly) {
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+
+    }
 }
