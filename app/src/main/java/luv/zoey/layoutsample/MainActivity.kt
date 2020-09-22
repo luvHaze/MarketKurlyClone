7e18ce4deedf8c348087294699c823c396fd7020
zoey <zooey4u@gmail.ccom>
2020-09-23 04:51:40 +0900

    레이아웃 활용

--- app/src/main/java/luv/zoey/layoutsample/MainActivity.kt
+++ app/src/main/java/luv/zoey/layoutsample/MainActivity.kt
@@ -12,6 +12,7 @@ import androidx.navigation.fragment.findNavController
 import androidx.navigation.ui.setupWithNavController
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.google.android.material.navigation.NavigationView
+import kotlinx.android.synthetic.main.actionbar_top.*
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity(), BottomNavigationView.OnNavigationItemSelectedListener {
@@ -28,7 +29,6 @@ class MainActivity : AppCompatActivity(), BottomNavigationView.OnNavigationItemS
 
     override fun onBackPressed() {
         //super.onBackPressed()
-
     }
 
     override fun onNavigationItemSelected(item: MenuItem): Boolean {
@@ -37,18 +37,29 @@ class MainActivity : AppCompatActivity(), BottomNavigationView.OnNavigationItemS
         when(item.title) {
             "홈" -> {
                 findNavController(R.id.nav_host_fragment_container).navigate(R.id.homeFragment2)
+                actionbar_logo.visibility = View.VISIBLE
+                actionbar_textview.visibility = View.INVISIBLE
                 return true
             }
             "카테고리" -> {
                 findNavController(R.id.nav_host_fragment_container).navigate(R.id.categoryFragment)
+                actionbar_logo.visibility = View.INVISIBLE
+                actionbar_textview.text = "카테고리"
+                actionbar_textview.visibility = View.VISIBLE
                 return true
             }
             "마이컬리" -> {
                 findNavController(R.id.nav_host_fragment_container).navigate(R.id.myKurlyFragment2)
+                actionbar_logo.visibility = View.INVISIBLE
+                actionbar_textview.text = "마이컬리"
+                actionbar_textview.visibility = View.VISIBLE
                 return true
             }
             "검색" -> {
                 findNavController(R.id.nav_host_fragment_container).navigate(R.id.searchFragment)
+                actionbar_logo.visibility = View.INVISIBLE
+                actionbar_textview.text = "검색"
+                actionbar_textview.visibility = View.VISIBLE
                 return true
             }
         }
